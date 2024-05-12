0b7e120ab5eaa6336c91592d8654131d34025e90
Rakesh Kumar Sahoo <sahoorakeshkumar42@gmail.com>
2024-05-12 10:51:20 +0530

    added input component

---
+++ src/components/Button.tsx
@@ -0,0 +1,28 @@
+import { ComponentPropsWithoutRef } from "react";
+
+interface Props extends ComponentPropsWithoutRef<'button'> {
+    children: string | JSX.Element | JSX.Element[];
+    type: "button" | "submit" | "reset";
+    bgColor: string;
+    textColor: string;
+    className: string;
+}
+
+function Button({
+    children,
+    type = "button",
+    bgColor = "bg-blue-600",
+    textColor = "text-white",
+    className = "",
+    ...props
+}: Props) {
+    return (
+        <button
+            type={type}
+            className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
+            {...props}
+        >{children}</button>
+    );
+}
+
+export default Button;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,71 @@
 import { Container, Logo, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../app/hooks";
 
 function Header() {
+    const authStatus = useAppSelector(state => state.auth.status);
+    const navigate = useNavigate();
+
+    const navItems: Array<{
+        name: string,
+        slug: string,
+        active: boolean;
+    }> = [
+            {
+                name: "Home",
+                slug: "/",
+                active: true
+            },
+            {
+                name: "Login",
+                slug: "/login",
+                active: !authStatus
+            },
+            {
+                name: "Signup",
+                slug: "/signup",
+                active: !authStatus
+            },
+            {
+                name: "All Posts",
+                slug: "/all-posts",
+                active: authStatus
+            },
+            {
+                name: "Add Post",
+                slug: "/add-post",
+                active: authStatus
+            }
+        ];
+
     return (
-        <div>Header</div>
+        <header className="py-3 shadow bg-gray-500">
+            <Container>
+                <nav className="flex">
+                    <div className="mr-4">
+                        <Link to="/">
+                            <Logo />
+                        </Link>
+                    </div>
+                    <ul className="flex ml-auto">
+                        {navItems.map(navItem => navItem.active ? <li key={navItem.name}>
+                            <button
+                                onClick={() => navigate(navItem.slug)}
+                                className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                            >
+                                {navItem.name}
+                            </button>
+                        </li> : null)}
+                    </ul>
+                    {authStatus && (
+                        <li>
+                            <LogoutBtn />
+                        </li>
+                    )}
+                </nav>
+            </Container>
+        </header>
     );
 }
 
